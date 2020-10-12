93f97d4e5df8514bd9e51c2f35f0398bf9e74560
Michael Mraka <michael.mraka@redhat.com>
2020-10-12 12:02:49 +0200

    added options to use podman/docker explicitly if needed
    and forced generator image update

--- generate-clients.sh
+++ generate-clients.sh
@@ -3,6 +3,18 @@
 CONTAINER=openapitools/openapi-generator-cli:latest
 CONTAINER_TOOL=$([ -x /usr/bin/podman ] && echo podman || echo docker)
 
+if [ -n "$1" ] ; then
+    case "$1" in
+        --docker) CONTAINER_TOOL=docker
+            ;;
+        --podman) CONTAINER_TOOL=podman
+            ;;
+        --help|*) echo "Usage: $0 [ --docker | --podman ]" >&2
+                  exit 1
+            ;;
+    esac
+fi
+
 # For now, we skip the dateTime parsing because of incompatible formats ( inventory does not produce fully compliant
 
 function generate_client() {
@@ -14,7 +26,7 @@ function generate_client() {
 
   HERE=$(pwd)
   # remove old code and doc
-  find "$HERE/$NAME" -name '*.go' -o -name '*.md' -print0 | xargs -0 rm
+  find "$HERE/$NAME" -name '*.go' -o -name '*.md' -print0 | xargs -0 rm -f
 
   $CONTAINER_TOOL run --rm -v ${HERE}:/local:z --security-opt=label=disable $CONTAINER generate \
       -i "/local/$LOCAL_JSON" \
@@ -27,6 +39,7 @@ function generate_client() {
 }
 
 
+$CONTAINER_TOOL pull $CONTAINER
 generate_client inventory "https://ci.cloud.redhat.com/api/inventory/v1/openapi.json"
 generate_client vmaas "https://webapp-vmaas-ci.5a9f.insights-dev.openshiftapps.com/api/openapi.json"
 generate_client rbac "https://ci.cloud.redhat.com/api/rbac/v1/openapi.json"
